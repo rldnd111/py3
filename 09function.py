6659795dd324d017c12d19edcd3e470df090d188
rldnd111 <80802801+rldnd111@users.noreply.github.com>
2021-07-09 12:49:51 +0900

    파이썬 함수 고급

--- 09function.py
+++ 09function.py
@@ -47,3 +47,135 @@ b = input('b는?')
 
 
 add()
+
+# 전역global 변수와 지역local 변수
+num = 10    # 10
+print(num)
+
+def local():
+    num = 20    # 20
+    print('함수내 num', num)
+
+local()
+
+print('전역변수 num', num) # 10
+
+# 함수내에서 전역변수 사용하기 : global
+num = 10    # 10
+print('전역변수 num', num)
+
+def local():
+    global num # 함수내에서 전역변수를 수정할 수 있도록 함
+    num = 20    # 20
+    print('함수내 num', num)
+
+local()
+
+print('전역변수 num', num) # 10(x)
+
+
+# 웹사이트 방문 누적하기
+count = 0
+
+def countVisitor(): # 함수정의
+    global count
+    # global count = 0 # 전역변수는 초기화 불가
+    while True:
+        join = int(input('1.웹 사이트 방문 2.종료'))
+        if join == 1:
+            count += 1
+            print(f'총 방문횟수 {count}')
+        elif join == 2:
+            print('웹 사이트 종료')
+            break
+
+countVisitor() # 함수호출
+
+
+x = 10
+y = 10
+
+def add():
+    print(x + y)
+
+def add(x, y):
+    print(x + y)
+
+
+add()
+add(10,10)
+
+
+# 함수의 매개변수 갯수를 동적으로 정의
+#
+
+# ex) 기존에 만든 add 함수는 2개의 매개변수만 받음
+# 2개이상의 매개변수를 받아 결과를 출력하고 싶다면?
+def add(x, y, z):
+    print(x + y + z)
+
+def add2(*num):
+    sum = 0
+    for v in num:
+        sum += v
+    print(sum)
+
+add2(10,10)
+add2(10,10,10,10)
+add2(10,10,10,10,10,10,10,10)
+
+
+def computeMS(msg):
+    msgSize = len(msg)
+    print('입력하신 문자열 길이', msgSize)
+
+    if msgSize <= 100:
+        print('단문 메세지로 50원이 부과됩니다')
+    elif msgSize > 100:
+        print('장문 메세지로 100원이 부과됩니다')
+
+
+msg = input('문자열을 입력하세요. ')
+computeMS(msg)
+
+# 함수정의시 매개변수를 선언했지만
+# 함수호출시 인수를 순서대로 입력하지 않을 경우
+# => 인수값 앞에 매개변수명을 지정
+
+def computeSungJuk(name, kor, eng, mat):
+    tot = kor + eng + mat
+    avg = tot / 3
+    print(tot, avg)
+
+computeSungJuk('혜교',98,78,65)
+computeSungJuk(98,78,65,'수지')    # 오류발생!
+computeSungJuk(98,78,65, name='수지')    # 오류발생!
+computeSungJuk(kor=98,eng=78,mat=65, name='수지')
+computeSungJuk(name='지현',kor=78,eng=65, mat=99)
+
+# 매개변수를 정의했지만
+# 매개변수 없이 함수 호출하고 싶다면?
+# -> 매개변수 선언시 초기값을 지정함
+
+def add3(x=10, y=10):
+    print(x + y)
+
+add3(5, 5)
+
+add3()
+
+
+def compute(x, y):
+    add = x + y
+    min = x - y
+    mul = x * y
+    div = x / y
+
+    return add, min, mul, div
+
+
+num1 = int(input('정수를 입력하세요'))
+num2 = int(input('정수를 입력하세요'))
+p, m, c, d = compute(num1, num2)
+
+print(f'사직연산 결과 : ({p}, {m}, {c}, {d:.4f})')
